498fce882bf3dfed091019adecb4a6f259aac7a4
Rodrigo Rojo <rodrigo.rojo@intel.com>
2020-09-10 12:56:31 -0700

    first CentOS7.6 RPM builder for OPAE

---
+++ README.md
@@ -0,0 +1,20 @@
+# CentOS 7 OPAE RPMs Action
+
+Use CentOS 7 to build OPAE RPMs
+
+## Inputs
+
+### `branch`
+
+**Optional** The branch in OPAE to checkout, default is `master`
+
+
+## Outputs
+
+
+
+## Example Usage
+
+uses: r-rojo/centos7-rpm
+with:
+  branch: master
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#! /bin/sh -l
+#
+# entrypoint.sh
+# Copyright (C) 2020 rrojo <rrojo@opae-ub16-01>
+#
+# Distributed under terms of the MIT license.
+#
+git clone https://github.com/OPAE/opae-sdk.git -b $1
+cd opae-sdk
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=$2  -DCPACK_GENERATOR=RPM -DOPAE_BUILD_LEGACY=ON
+make package_rpm -j $(nproc)
